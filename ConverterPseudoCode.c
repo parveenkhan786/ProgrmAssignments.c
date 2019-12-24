f9efd73c347fa9a4c1e2c919e096107c1ba6e17f
Parveenkhan <56950494+parveenkhan786@users.noreply.github.com>
2019-12-24 12:33:58 +0500

    Create ConverterPseudoCode.c

---
+++ ConverterPseudoCode.c
@@ -0,0 +1,17 @@
+ Display a message "Enter amount".
+
+2. Take input from the user.
+
+3. Display available choices.
+
+4. Take input from user. (make choice)
+
+5. Perform conversion.
+
+6. If (choice is 2) then,
+7.    result = 
+
+8. If (choice is 3) then,
+9.   result =
+
+10. Display result.
